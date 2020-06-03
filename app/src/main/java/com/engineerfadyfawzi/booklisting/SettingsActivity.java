550da2fa9becf83586205fbb5299c09953032d8a
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-06-03 19:08:17 +0200

    1.15.2 Create the SettingsActivity class (SettingsActivity.java) and Define it's layout (settings_activity.xml)

---
+++ app/src/main/java/com/engineerfadyfawzi/booklisting/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.engineerfadyfawzi.booklisting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_settings );
+    }
+    
+    public static class BookPreferenceFragment extends PreferenceFragment
+    {
+    
+    }
+}
